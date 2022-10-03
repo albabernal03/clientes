30f065bff14d80840bc9c01d04d2b087295ad9c0
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 12:04:40 +0200

    seguimos cambiando

--- gestor/menu.py
+++ gestor/menu.py
@@ -26,18 +26,16 @@ def iniciar():
             print('Buscando un cliente...\n')
             dni= helpers.leer_texto(3,3,'DNI (2 ints y 1 char').upper()
             cliente= db.Clientes.buscar(dni)
+            
         if opcion == '3':
             print('Añadiendo un cliente...\n')
-            dni = None
-            while True:
-                dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-                if helpers.dni_valido(dni, db.Clientes.lista):
-                    break
+            dni= helpers.leer_texto(3,3,'DNI (2 ints y 1 char').upper()
+            nombre= helpers.leer_texto(2,30,'Nombre (de 2 a 30 chars').upper()
+            apellido= helpers.leer_texto(2,30,'Apellido (de 2 a 30 chars').upper()
+            db.Clientes.crear(dni,nombre,apellido)
+            print('Cliente añadido correctamente')
 
-            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
-            apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
-            print("Cliente añadido correctamente.")
+
         if opcion == '4':
             print('Modificando un cliente...\n')
         if opcion == '5':
