a39db6d0788d9519e67664308491bd9ab2208309
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 10:14:26 +0200

    creamos distintas funciones las cuales estan encargadas de eliminar, buscar...

--- Gestor_clientes/database.py
+++ Gestor_clientes/database.py
@@ -6,3 +6,27 @@ class Cliente:
     
     def __str__(self):
         return f"{self.dni},{self.nombre},{self.apellido}" #esta funcion es para que cuando se imprima el objeto, se imprima en el formato que yo quiero
+
+class Clientes:
+    lista=[]
+
+    @staticmethod #para que no se cree una instancia de la clase
+    def buscar(dni):
+        for cliente in Clientes.lista:
+            if cliente.dni == dni:
+                return cliente
+    
+    @staticmethod
+    def crear(dni,nombre,apellido):
+        cliente = Cliente(dni,nombre,apellido)
+        Clientes.lista.append(cliente)
+        return cliente
+    
+    @staticmethod
+    def modificar(dni,nombre,apellido):
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                return Clientes.lista[i]
+        
