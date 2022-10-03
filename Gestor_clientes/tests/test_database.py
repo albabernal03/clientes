0203b368ff6a166f5037fcc7ff8eae5c325692a0
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 10:32:49 +0200

    modificando el test_database.py

--- Gestor_clientes/tests/test_database.py
+++ Gestor_clientes/tests/test_database.py
@@ -2,3 +2,12 @@ import copy # Para copiar objetos
 import unittest # Para hacer pruebas unitarias
 import database as db # Para importar el modulo database
 
+class TestDatabase(unittest.TestCase):
+    def setUp(self): # Se ejecuta antes de cada prueba
+        db.Clientes.lista = [
+            db.Cliente('15J', 'Marta', 'Pérez'),
+            db.Cliente('48H', 'Manolo', 'López'),
+            db.Cliente('28Z', 'Ana', 'García')
+        ]
+    def test_bucar_cliente(self):
+        
