e38b2ae5a1fcd7c80780fdfa090d6e9619470585
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 10:37:16 +0200

    funcion para crear

--- Gestor_clientes/tests/test_database.py
+++ Gestor_clientes/tests/test_database.py
@@ -10,4 +10,15 @@ class TestDatabase(unittest.TestCase):
             db.Cliente('28Z', 'Ana', 'García')
         ]
     def test_bucar_cliente(self):
-        
+        cliente_existe = db.Clientes.buscar('15j') 
+        cliente_no_existe = db.Clientes.buscar('99X')
+        self.assertIsNotNone(cliente_existe)
+        self.assertIsNone(cliente_no_existe)
+
+    def test_crear_cliente(self):
+        nuevo_cliente = db.Clientes.crear('39X', 'Héctor', 'Costa')
+        self.assertEqual(len(db.Clientes.lista), 4)
+        self.assertEqual(nuevo_cliente.dni, '39X')
+        self.assertEqual(nuevo_cliente.nombre, 'Héctor')
+        self.assertEqual(nuevo_cliente.apellido, 'Costa')
+
