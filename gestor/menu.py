d4cb85991b7c96893e93f9dca139fc4224af30b2
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 12:15:21 +0200

    cambios hechos

--- gestor/menu.py
+++ gestor/menu.py
@@ -26,7 +26,7 @@ def iniciar():
             print('Buscando un cliente...\n')
             dni= helpers.leer_texto(3,3,'DNI (2 ints y 1 char').upper()
             cliente= db.Clientes.buscar(dni)
-            
+
         if opcion == '3':
             print('Añadiendo un cliente...\n')
             dni= helpers.leer_texto(3,3,'DNI (2 ints y 1 char').upper()
@@ -38,8 +38,23 @@ def iniciar():
 
         if opcion == '4':
             print('Modificando un cliente...\n')
+            dni= helpers.leer_texto(3,3,'DNI (2 ints y 1 char').upper()
+            cliente = db.Clientes.buscar(dni)
+            if cliente:
+                nombre = helpers.leer_texto(
+                    2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
+                apellido = helpers.leer_texto(
+                    2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
+                db.Clientes.modificar(cliente.dni, nombre, apellido)
+                print("Cliente modificado correctamente.")
+            else:
+                print("Cliente no encontrado.")
         if opcion == '5':
             print('Borrando un cliente...\n')
+            dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+            print("Cliente borrado correctamente.") if db.Clientes.borrar(
+                dni) else print("Cliente no encontrado.")
+
         if opcion == '6':
             print('Saliendo...\n')
             break
