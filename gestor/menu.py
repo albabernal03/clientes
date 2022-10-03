51dfdaf3e668433730b784c2019c741bdcbab0cc
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 11:35:35 +0200

    Terminamos de modificar el menu

--- gestor/menu.py
+++ gestor/menu.py
@@ -12,3 +12,19 @@ def iniciar():
         print("[5] Borrar un cliente   ")
         print("[6] Cerrar el Gestor    ")
         print("========================")
+
+        opcion= input('>')
+        if opcion == '1':
+            print('Listando los clientes...\n')
+        if opcion == '2':
+            print('Buscando un cliente...\n')
+        if opcion == '3':
+            print('Añadiendo un cliente...\n')
+        if opcion == '4':
+            print('Modificando un cliente...\n')
+        if opcion == '5':
+            print('Borrando un cliente...\n')
+        if opcion == '6':
+            print('Saliendo...\n')
+            break
+        input('\n Presione ENTER para continuar...')
