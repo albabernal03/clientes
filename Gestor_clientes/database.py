91f0c39eb7e05e329146a438be4a0f2f91c86966
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-03 10:05:19 +0200

    creeamos una funcion para que el objeto se imprima de una determinada forma

--- Gestor_clientes/database.py
+++ Gestor_clientes/database.py
@@ -3,3 +3,6 @@ class Cliente:
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
+    
+    def __str__(self):
+        return f"{self.dni},{self.nombre},{self.apellido}" #esta funcion es para que cuando se imprima el objeto, se imprima en el formato que yo quiero
